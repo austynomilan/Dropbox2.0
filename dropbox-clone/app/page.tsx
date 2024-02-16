3cffd66895630b15ce8a4c0d9a4112ae2afd61b9
Augustine Odoh O <62001398+austynomilan@users.noreply.github.com>
2024-02-16 20:22:43 +0000

    clerk installed for auth and shahcdn for ui

--- dropbox-clone/app/page.tsx
+++ dropbox-clone/app/page.tsx
@@ -1,9 +1,23 @@
-import { UserButton } from "@clerk/nextjs";
+import { UserButton } from '@clerk/nextjs';
+import { ArrowRight } from 'lucide-react';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className="">
-      <h1>Drop box</h1>
+    <main className=''>
+      <div>
+        <div className='p-10 flex flex-col'>
+          <h1 className='text-5xl font-bold bg-[#2B2929] dark:bg-slate-800 text-white space-y-5   '>
+            Welcome to Dropbox. <br />
+            Storing everything for you and your business needs. All in one place
+          </h1>
+
+          <Link href='/dashboard' className='flex '>
+            Try it for free!
+            <ArrowRight className='ml-5' />
+          </Link>
+        </div>
+      </div>
 
       <UserButton />
     </main>
--- dropbox-clone/components/Header.tsx
+++ dropbox-clone/components/Header.tsx
@@ -1,21 +1,34 @@
-import Link from "next/link";
-import Image from "next/image";
+import Link from 'next/link';
+import Image from 'next/image';
+import { SignInButton, SignedOut, UserButton } from '@clerk/nextjs';
+import { ThemeToggler } from './ThemeToggler';
 
 function Header() {
-  return <header className="flex justify-between items-center">
-<Link href="/">
-    <div>
-        <Image 
-        src="https://www.shareicon.net/data/128x128/2015/11/08/668675_box_512x512.png"
-        alt="logo"
-        className=""
-        height={50}
-        width={50}
-        />
-    </div>
-    <h1 className="font-bold text-xl">Dropbox</h1>
-</Link>
-    </header>;
+  return (
+    <header className='flex justify-between items-center'>
+      <Link href='/' className='flex items-center space-x-2'>
+        <div className='bg-[#0160fe] w-fit'>
+          <Image
+            src='https://www.shareicon.net/data/128x128/2015/11/08/668675_box_512x512.png'
+            alt='logo'
+            className='invert'
+            height={50}
+            width={50}
+          />
+        </div>
+        <h1 className='font-bold text-xl'>Dropbox</h1>
+      </Link>
+
+      <div className='flex space-x-2 items-center px-5'>
+        <ThemeToggler />
+
+        <UserButton afterSignOutUrl='/' />
+        <SignedOut>
+          <SignInButton afterSignInUrl='/dashboard' mode='modal' />
+        </SignedOut>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
