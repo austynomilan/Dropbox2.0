189a3c6c08036934a81dbf0625a4b0cdda71f959
Augustine Odoh O <62001398+austynomilan@users.noreply.github.com>
2024-02-19 19:45:34 +0000

    feat: download url of uploaded image retrieved

--- dropbox-clone/app/dashboard/page.tsx
+++ dropbox-clone/app/dashboard/page.tsx
@@ -1,12 +1,39 @@
 import Dropzone from '@/components/Dropzone';
-import { auth } from '@clerk/nextjs'
+import TableWrapper from '@/components/Table/TableWrapper';
+import { db } from '@/firebase';
+import { FileType } from '@/typings';
+import { auth } from '@clerk/nextjs';
+import { collection, getDocs } from 'firebase/firestore';
 
-
-export default function Dashboard() {
+async function Dashboard() {
   const { userId } = auth();
+
+  const docsResult = await getDocs(collection(db, 'users', userId!, 'file'));
+  const skeletonFiles: FileType[] = docsResult.docs.map((doc) => ({
+    id: doc.id,
+    fileName: doc.data().fileName || doc.id,
+    timeStamp: new Date(doc.data().timestamp?.seconds * 1000) || undefined,
+    fullName: doc.data().fullName,
+    downloadUrl: doc.data().downloadURL,
+    type: doc.data().type,
+    size: doc.data().size,
+  }));
+
   return (
-    <div>
+    <div className='boarder-'>
       <Dropzone />
+
+      <section className='container space-y-5'>
+        <h2 className='font-bold'>All Files</h2>
+
+        <div>
+          <TableWrapper 
+          skeletonFiles = {skeletonFiles}
+          />
+        </div>
+      </section>
     </div>
-  )
+  );
 }
+
+export default Dashboard;
---
+++ dropbox-clone/constants.ts
@@ -0,0 +1,15 @@
+export const COLOR_EXTENSION_MAP: { [Key: string]: string } = {
+  pdf: '#ff0000', // red
+  doc: '#0000ff', // blue
+  docx: '#0000ff', // blue
+  txt: '#00ff00', // green
+  jpg: '#ff00ff', // magenta
+  png: '#ff00ff', // magenta
+  xls: '#ffff00', // yellow
+  xlsx: '#ffff00', // yellow
+  ppt: '#ffa500', // orange
+  pptx: '#ffa500', // orange
+  zip: '#808080', // gray
+  rar: '#808080', // gray
+  default: '#000000', // black (default color)
+};
